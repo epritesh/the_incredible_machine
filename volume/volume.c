a9bdd5c7f83e21326865551cf1b0f1f954b32926
epritesh <epritesh@gmail.com>
2025-09-09 15:52:55 +0000

    Tue, Sep 9, 2025, 11:52 AM -04:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,15 +35,14 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fread(header, 1, HEARDER_SIZE, input);
-    fwrite(header, 1, HEARDER_SIZE, output);
+    fread(header, 1, HEADER_SIZE, input);
+    fwrite(header, 1, HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    fread(&buffer,2,1,input);
+    fread(&buffer, 2, 1, input);
     buffer *= factor;
-    fwrite(b)
-
+    fwrite(&buffer, 2, 1, output);
 
     // Close files
     fclose(input);
