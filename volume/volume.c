495e12ce339741bb6ee2babdb7e57fc56307f6f0
epritesh <epritesh@gmail.com>
2025-09-09 15:42:57 +0000

    Tue, Sep 9, 2025, 11:42 AM -04:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,6 +41,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
     fread(&buffer,2,1,input);
+    buffer *= 
     fwrite()
 
 
