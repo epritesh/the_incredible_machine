d863416c99049adb78a060a3ed7ffd024106432b
epritesh <epritesh@gmail.com>
2025-09-11 14:44:01 +0000

    Thu, Sep 11, 2025, 10:44 AM -04:00

--- sentimental-mario-less/mario-less.py
+++ sentimental-mario-less/mario-less.py
@@ -1,6 +1,6 @@
 # Prompt the user for input and store it as an integer
 height = int(input("Height: "))
 
-while (height >=1 or height >= 9):
+while (height <=1 or height <= 9):
     height = int(input("Height: "))
 print(height)
