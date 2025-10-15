547322e0e8f8c25a92ae4ba04aa48f972d98453f
epritesh <epritesh@gmail.com>
2025-10-15 09:08:49 -0400

    Make Goal draw as pulsing outline and use body position for isInside

--- src/entities/goal.lua
+++ src/entities/goal.lua
@@ -4,6 +4,8 @@ local Goal = {
     type = "goal",
     width = 64,
     height = 64,
+    color = {0, 1, 0},
+    pulseTimer = 0,
 }
 setmetatable(Goal, Base)
 Goal.__index = Goal
@@ -15,13 +17,24 @@ function Goal:new(data)
 end
 
 function Goal:draw()
-    love.graphics.setColor(0, 1, 0, 0.5)
-    love.graphics.rectangle("fill", self.x - self.width/2, self.y - self.height/2, self.width, self.height)
+    -- pulsing outline to make goal obvious but not visually blocking
+    self.pulseTimer = (self.pulseTimer or 0) + (love.timer.getDelta() or 0.016)
+    local pulse = 0.5 + 0.5 * math.sin(self.pulseTimer * 2)
+    local r, g, b = unpack(self.color)
+    love.graphics.setLineWidth(3)
+    love.graphics.setColor(r, g, b, 0.6 * pulse)
+    love.graphics.rectangle("line", self.x - self.width/2, self.y - self.height/2, self.width, self.height)
 end
 
 function Goal:isInside(obj)
-    return obj.x > self.x - self.width/2 and obj.x < self.x + self.width/2 and
-           obj.y > self.y - self.height/2 and obj.y < self.y + self.height/2
+    local ox, oy
+    if obj.body and obj.body.getX then
+        ox, oy = obj.body:getPosition()
+    else
+        ox, oy = obj.x, obj.y
+    end
+    return ox > self.x - self.width/2 and ox < self.x + self.width/2 and
+           oy > self.y - self.height/2 and oy < self.y + self.height/2
 end
 
 return Goal
